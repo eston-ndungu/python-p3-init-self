3bca3880835318a069abd8ff556403a81e1cce1d
ESTON NDUNGU <estonndungu1@gmail.com>
2024-09-01 16:20:10 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    #define an _init_ method
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        
